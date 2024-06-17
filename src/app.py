ce2dfe9f76ab60b101e9da762be3202686a94a44
Jose Antonio Guzman <guzmanjose639@yahoo.com>
2024-06-17 23:34:54 +0000

    version updates in requirments.txt

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine
+from SQLAlchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
 
@@ -7,6 +7,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
+
+DATABASE_TYPE = os.getenv('DB_NAME')
+USERNAME = os.getenv('DB_USER')
+PASSWORD = os.getenv('DB_PASSWORD')
+HOST = os.getenv('DB_HOST')
+PORT = os.getenv('DB_PORT')
+DATABASE = os.getenv('DB_NAME')
+
+
+
+# Construct the database URL
+DATABASE_URL = f"{DATABASE_TYPE}://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+
+# Create the engine
+engine = create_engine(DATABASE_URL)
+
+try:
+    with engine.connect() as connection:
+        print("Connection to the PostgreSQL database was successful!")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
